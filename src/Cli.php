68f5b84f953f863c351148c308ec13a1a87c42f9
svyatik44 <svyat.2807@gmail.com>
2022-07-11 13:30:04 +0300

    step 5

---
+++ src/Cli.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Description
+ *
+ * PHP version 8
+ *
+ * @category Cli
+ * @package  WordPress_Custom_Fields_Permalink
+ * @author   Display Name <svyat.2807@gmail.com>
+ * @license  https://github.com/svyatik44/ svyatik44
+ * @link     https://github.com/svyatik44/
+ */
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+/**
+ * Ask the name().
+ *
+ * Description
+ *
+ * @return void An array of menu items
+ */
+function welcome()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
