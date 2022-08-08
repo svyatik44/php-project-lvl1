955b29815f4822d753a998145b53828b7f5c23ae
svyatik44 <ukrainecsvatoslav85@gmail.com>
2022-08-08 12:24:43 +0300

    fix linter

--- src/Games/ToPrime.php
+++ src/Games/ToPrime.php
@@ -25,13 +25,15 @@ use function Brain\Games\Engine\runGame;
  */
 function isPrime($num)
 {
-    if($num == 1) return false;
+    if ($num == 1) {
+        return false;
+    }
 
     $flag = true;
     for ($i = 2; $i < $num; $i++) {
         if (($num % $i) === 0) {
             $flag = false;
-        } 
+        }
     }
     return $flag;
 }
